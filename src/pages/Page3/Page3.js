83b02dbbff22a032750bb84f3b3576cefdf7bb34
Panda <258730525@qq.com>
2018-03-10 15:16:55 +0800

    * 提交

---
+++ src/pages/Page3/Page3.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import 'pages/css/headbar.css';
+
+export default class Page3 extends Component {
+	render() {
+		return (
+			<div className="a">
+				this is Page3~
+			</div>
+		)
+	}
+}
