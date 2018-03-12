55651ac24f14c62d74a48f71ce0a3d43416ddec0
Panda <258730525@qq.com>
2018-03-12 11:35:13 +0800

    * 重置

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,6 @@ export default class Nav extends Component {
             <ul>
                 <li><Link to="/">首页</Link></li>
                 <li><Link to="/page1">Page1</Link></li>
-				<li><Link to="/page2">Page2</Link></li>
                 <li><Link to="/counter">Counter</Link></li>
                 <li><Link to="/userinfo">UserInfo</Link></li>
             </ul>
